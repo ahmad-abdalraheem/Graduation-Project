929f6b1028485ff74200d2eb37140b49dc06a0f5
mahmoud661 <mahmoudzuriqi8@gmail.com>
2024-10-19 23:53:06 +0300

    test

--- backEnd/main.py
+++ backEnd/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # app.include_router(course.router, prefix="/api/v1/courses", tags=["Courses"])
 # app.include_router(user.router, prefix="/api/v1/users", tags=["Users"])
 
-
+#dfgd
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Job Position Roadmap Recommender API"}
