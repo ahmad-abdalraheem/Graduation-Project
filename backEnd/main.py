a32fad9201fc353da6e79eb15628ecb4016fbb4d
mahmoud661 <mahmoudzuriqi8@gmail.com>
2024-10-21 18:00:32 +0300

    GP-9 OAuth 2 Markdown

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,16 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status, HTTPException
+from fastapi.security import OAuth2AuthorizationCodeBearer, OAuth2, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-
-#from routes import auth, course, user
+from routes import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-#app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
-# Uncomment if you have course and user implemented
+
+app.add_middleware(
+    CORSMiddleware,
+    # Allows all origins, but be more restrictive in production
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 # app.include_router(course.router, prefix="/api/v1/courses", tags=["Courses"])
 # app.include_router(user.router, prefix="/api/v1/users", tags=["Users"])
 
-#dfgd
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Job Position Roadmap Recommender API"}
