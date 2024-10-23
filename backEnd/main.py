18c3e54ab321e31702760baba0bc930799d71562
mahmoud661 <mahmoudzuriqi8@gmail.com>
2024-10-23 20:34:58 +0300

    Discarding the work done on the spike branch

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,25 +1,16 @@
-from fastapi import FastAPI, Depends, status, HTTPException
-from fastapi.security import OAuth2AuthorizationCodeBearer, OAuth2, OAuth2PasswordRequestForm
+from fastapi import FastAPI
 from pydantic import BaseModel
-from routes import auth
-from fastapi.middleware.cors import CORSMiddleware
+
+# from routes import auth, course, user
 
 app = FastAPI()
 
-
-app.add_middleware(
-    CORSMiddleware,
-    # Allows all origins, but be more restrictive in production
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+# app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
+# Uncomment if you have course and user implemented
 # app.include_router(course.router, prefix="/api/v1/courses", tags=["Courses"])
 # app.include_router(user.router, prefix="/api/v1/users", tags=["Users"])
+
+# dfgd
 
 
 @app.get("/")
