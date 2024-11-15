024d93d391d87785f27f206539599333facb8d26
ahmad-abdelraheem <ahmadabdelraheem@outlook.com>
2024-11-15 13:41:42 +0300

    Temp Commit

--- FrontEnd/DevPaths/src/components/Header/index.tsx
+++ FrontEnd/DevPaths/src/components/Header/index.tsx
@@ -4,19 +4,22 @@ import style from "./Header.module.scss";
 export const Header = () => {
   return (
     <div className={style.header}>
-      <div className={style.brand}>
-        <img src="public/logo.png" alt="Dev Path Logo" />
-        <h3>Dev Path</h3>
-      </div>
+      <h3 className={style.brand}>Dev Path</h3>
+
       <nav className={style.navMenu}>
         <a href="">home</a>
         <a href="">roadmap</a>
         <a href="">My Progress</a>
         <a href="">about us</a>
       </nav>
-      <div className={style.searchBar + " hidden-lg"}>
-      <i className="bi bi-search"></i>
-      </div>
+      <menu className={style.actions}>
+        <button className={style.loginButton + " " + style.button}>
+          Login
+        </button>
+        <button className={style.registerButton + " " + style.button}>
+          Register
+        </button>
+      </menu>
     </div>
   );
 };
